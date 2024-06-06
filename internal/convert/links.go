c5a87692fc3e1d10e0d84470b42f90d37520fc65
Mohammad Banisaeid <smbl64@gmail.com>
2024-06-06 12:34:09 +0200

    Extract base git path from the remote URL

--- internal/convert/links.go
+++ internal/convert/links.go
@@ -5,16 +5,14 @@ import (
 	"regexp"
 )
 
-const basePath = "https://gitlab.example.org"
-
 var regz []*regexp.Regexp = []*regexp.Regexp{
 	// Matches this format:
 	// git@gitlab.example.org:team/subteam/project.git
-	regexp.MustCompile(`^git@gitlab.example.org:(?P<path>.+)\.git$`),
+	regexp.MustCompile(`^git@(?P<base>.+):(?P<path>.+)\.git$`),
 	// Matches these formats:
 	// https://gitlab.example.org/team/project
 	// https://gitlab.example.org/team/project.git
-	regexp.MustCompile(`^https://gitlab.example.org/(?P<path>.+?)(\.git)?$`),
+	regexp.MustCompile(`^https://(?P<base>.+)/(?P<path>.+?)(\.git)?$`),
 }
 
 func ConvertGitRemoteToHTTP(remoteURL string) (string, error) {
@@ -23,7 +21,10 @@ func ConvertGitRemoteToHTTP(remoteURL string) (string, error) {
 		if len(parts) > 0 {
 			pathIdx := re.SubexpIndex("path")
 			path := parts[pathIdx]
-			return fmt.Sprintf("%s/%s", basePath, path), nil
+
+			pathIdx = re.SubexpIndex("base")
+			base := parts[pathIdx]
+			return fmt.Sprintf("https://%s/%s", base, path), nil
 		}
 	}
 
